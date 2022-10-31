b8f2031def87d2a333525a2313b0adfecb3fbf34
PR <contact@dividebyzeno.com>
2022-10-31 03:40:46 -0400

    Added new Entity Framework .NET 6 project for working with common app models.

--- ZenoWeatherAppData/Program.cs
+++ ZenoWeatherAppData/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -11,14 +13,117 @@ namespace ZenoWeatherAppData
     {
         static void Main(string[] args)
         {
+            using (var db = new WeatherContext())
+            {
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Location: ");
+                var name = Console.ReadLine();
+
+                var location = new Location { LocalizedName = name };
+                db.Locations.Add(location);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.Locations
+                            orderby b.LocalizedName
+                            select b;
+
+                Console.WriteLine("All locations in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.LocalizedName);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
+    }
+
+    public class Country
+    {
+        public string CountryId
+        {
+            get; set;
+        }
+
+        public string LocalizedName
+        {
+            get; set;
+        }
+
+        public int LocationId
+        {
+            get; set;
+        }
+    }
+
+    public class AdministrativeArea
+    {
+        public string AdministrativeAreaId
+        {
+            get; set;
+        }
+
+        public string LocalizedName
+        {
+            get; set;
+        }
+
+        public int LocationId
+        {
+            get; set;
+        }
+    }
+
+    public class Location
+    {
+        public int LocationId
+        {
+            get; set;
+        }
+
+        public string Key
+        {
+            get; set;
+        }
+
+        public string Type
+        {
+            get; set;
+        }
+
+        public string LocalizedName
+        {
+            get; set;
+        }
+
+        public string PrimaryPostalCode
+        {
+            get; set;
+        }
+
+        public Country Country
+        {
+            get; set;
+        }
+
+        public AdministrativeArea AdministrativeArea
+        {
+            get; set;
+        }
+
+        //public List<string> DataSets
+        //{
+        //    get; set;
+        //}
     }
 
     public class WeatherContext : DbContext
     {
-        //public DbSet<Blog> Blogs
-        //{
-        //    get; set;
-        //}
+        public DbSet<Location> Locations
+        {
+            get; set;
+        }
     }
 }
---
+++ ZenoWeatherAppDataNet6/Program.cs
@@ -0,0 +1,131 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZenoWeatherAppDataNet6;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        using (var conn = new SqlConnection("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=ZenoWeatherAppData.WeatherContext;Integrated Security=True"))
+        {
+            conn.Open();
+
+            var sqlCommand = new SqlCommand();
+            sqlCommand.Connection = conn;
+            sqlCommand.CommandText =
+                @"SELECT * FROM Locations;";
+            sqlCommand.ExecuteNonQuery();
+
+            using (var context = new WeatherContext(conn.ConnectionString))
+            {
+                foreach (var c in context.Locations)
+                    Console.WriteLine(c.LocalizedName);
+            }
+        }
+    }
+
+    public class Country
+    {
+        public string? CountryId
+        {
+            get; set;
+        }
+
+        public string? LocalizedName
+        {
+            get; set;
+        }
+
+        public int LocationId
+        {
+            get; set;
+        }
+    }
+
+    public class AdministrativeArea
+    {
+        public string? AdministrativeAreaId
+        {
+            get; set;
+        }
+
+        public string? LocalizedName
+        {
+            get; set;
+        }
+
+        public int LocationId
+        {
+            get; set;
+        }
+    }
+
+    public class Location
+    {
+        public int LocationId
+        {
+            get; set;
+        }
+
+        public string? Key
+        {
+            get; set;
+        }
+
+        public string? Type
+        {
+            get; set;
+        }
+
+        public string? LocalizedName
+        {
+            get; set;
+        }
+
+        public string? PrimaryPostalCode
+        {
+            get; set;
+        }
+
+        public Country? Country
+        {
+            get; set;
+        }
+
+        public AdministrativeArea? AdministrativeArea
+        {
+            get; set;
+        }
+
+        //public List<string> DataSets
+        //{
+        //    get; set;
+        //}
+    }
+
+    public class WeatherContext : DbContext
+    {
+        string conn = "";
+        public WeatherContext(string connectionString)
+        : base()
+        {
+            conn = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (options.IsConfigured == false)
+                options.UseSqlServer(conn);
+        }
+
+        public DbSet<Location>? Locations
+        {
+            get; set;
+        }
+    }
+}
