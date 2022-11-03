576649ec4adbd3d9ee6b3c66990b1074f4997f48
PR <contact@dividebyzeno.com>
2022-11-03 05:09:35 -0400

    Draft UI for ASP .NET version of weather app

---
+++ ZenoWeatherAppWeb/TagHelpers/ActivePageTagHelper.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ZenoWeatherAppWeb.TagHelpers;
+// You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+
+/// <summary>
+/// Tag Helper to mark active navigation item,
+/// source: https://gist.github.com/DanElliott/32787b4ae1941780d70cb085d55f8b24
+/// </summary>
+[HtmlTargetElement(Attributes = "is-active-page")]
+public class ActivePageTagHelper : TagHelper
+{
+    /// <summary>The name of the action method.</summary>
+    /// <remarks>Must be <c>null</c> if <see cref="P:Microsoft.AspNetCore.Mvc.TagHelpers.AnchorTagHelper.Route" /> is non-<c>null</c>.</remarks>
+    [HtmlAttributeName("asp-page")]
+    public string Page
+    {
+        get; set;
+    }
+
+    /// <summary>
+    /// Gets or sets the <see cref="T:Microsoft.AspNetCore.Mvc.Rendering.ViewContext" /> for the current request.
+    /// </summary>
+    [HtmlAttributeNotBound]
+    [ViewContext]
+    public ViewContext ViewContext
+    {
+        get; set;
+    }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        base.Process(context, output);
+
+        if (ShouldBeActive())
+        {
+            MakeActive(output);
+        }
+
+        output.Attributes.RemoveAll("is-active-page");
+    }
+
+    private bool ShouldBeActive()
+    {
+        string currentPage = ViewContext.RouteData.Values["Page"].ToString();
+
+        if (!string.IsNullOrWhiteSpace(Page) && Page.ToLower() != currentPage.ToLower())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void MakeActive(TagHelperOutput output)
+    {
+        var classAttr = output.Attributes.FirstOrDefault(a => a.Name == "class");
+        if (classAttr == null)
+        {
+            classAttr = new TagHelperAttribute("class", "active");
+            output.Attributes.Add(classAttr);
+        }
+        else if (classAttr.Value == null || classAttr.Value.ToString().IndexOf("active") < 0)
+        {
+            output.Attributes.SetAttribute("class", classAttr.Value == null
+                ? "active"
+                : classAttr.Value.ToString() + " active");
+        }
+    }
+}
