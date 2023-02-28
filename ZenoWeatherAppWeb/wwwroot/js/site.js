b89d55f1c3af3ab13be3801b0541ea7ee6b15641
PR <contact@dividebyzeno.com>
2023-02-28 00:54:31 -0500

    Replaced old header with Bootstrap offcanvas navbar; small improvements

--- ZenoWeatherAppWeb/wwwroot/js/site.js
+++ ZenoWeatherAppWeb/wwwroot/js/site.js
@@ -4,50 +4,5 @@
 // Write your JavaScript code.
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    const showNavbar = (toggleId, toggleId2, navId, bodyId, headerId) => {
-        const toggle = document.getElementById(toggleId),
-            toggle2 = document.getElementById(toggleId2),
-            nav = document.getElementById(navId),
-            bodypd = document.getElementById(bodyId),
-            headerpd = document.getElementById(headerId)
 
-        // Validate that all variables exist
-        if (toggle && toggle2 && nav && bodypd && headerpd) {
-            toggle.addEventListener('click', () => {
-                // show navbar
-                nav.classList.toggle('show')
-                // change icon
-                toggle.classList.toggle('bx-x')
-                // add padding to body
-                bodypd.classList.toggle('body-pd')
-                // add padding to header
-                headerpd.classList.toggle('body-pd')
-            })
-            toggle2.addEventListener('click', () => {
-                // show navbar
-                nav.classList.toggle('show')
-                // change icon
-                toggle.classList.toggle('bx-x')
-                // add padding to body
-                bodypd.classList.toggle('body-pd')
-                // add padding to header
-                headerpd.classList.toggle('body-pd')
-            })
-        }
-    }
-
-    showNavbar('header-toggle', 'header-toggle-sidebar', 'nav-bar', 'body-pd', 'header')
-
-    /*===== LINK ACTIVE =====*/
-    const linkColor = document.querySelectorAll('.nav_link')
-
-    function colorLink() {
-        if (linkColor) {
-            linkColor.forEach(l => l.classList.remove('active'))
-            this.classList.add('active')
-        }
-    }
-    linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-    // Your code to run since DOM is loaded and ready
 });
